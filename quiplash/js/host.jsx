2e547200b7dc9afe83293184e047021c146638ec
Brett Levenson <brettlev@umich.edu>
2018-12-26 13:31:57 -0800

    Show current players that have joined

--- quiplash/js/host.jsx
+++ quiplash/js/host.jsx
@@ -11,6 +11,30 @@ class Host extends React.Component {
   }
 
   componentDidMount() {
+    this.interval = setInterval(() => this.getPlayers(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  getPlayers() {
+
+    fetch('/api/v1/players', {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'same-origin',
+    })
+    .then((response) => {
+        if (!response.ok) throw Error(response.statusText);
+        return response.json();
+      }).then((data) => {
+        this.setState(prevState => ({
+          players: data.players,
+        }));
+      })
+      .catch();
+
   }
 
 
@@ -20,7 +44,7 @@ class Host extends React.Component {
         <p>Waiting for all players...</p>
         <ul>
           {
-          this.props.players.map((player, index) =>
+          this.state.players.map((player, index) =>
             <li key={index}><p>{player.name}</p></li>)
             }
         </ul>
