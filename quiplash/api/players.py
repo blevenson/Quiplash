ecf511485e0dee15354e672c9305a2861b1bc1fc
Brett Levenson <brettlev@umich.edu>
2018-12-26 17:30:42 -0800

    Player state for when players have all answered questions

--- quiplash/api/players.py
+++ quiplash/api/players.py
@@ -27,3 +27,26 @@ def get_players():
     context["players"] = output
 
     return flask.jsonify(**context)
+
+
+@quiplash.app.route('/api/v1/resetanswer', methods=["GET"])
+def reset_answers():
+    """Reset answers for all players.
+    """
+
+    context = {}
+
+    # url
+    context["url"] = flask.request.path
+
+    # Database
+    db = quiplash.model.get_db()
+
+    cur = db.execute(('UPDATE players SET ans1 = \'\', ans2 = \'\' '))
+
+    cur = db.execute("SELECT * FROM players",)
+    output = cur.fetchall()
+
+    context["players"] = output
+
+    return flask.jsonify(**context)
--- quiplash/js/host.jsx
+++ quiplash/js/host.jsx
@@ -6,7 +6,12 @@ class Host extends React.Component {
 
     this.state = {
       players: [],
+      stage: 0,
+      answeredPlayers: [],
+      votedPlayers: [],
     };
+
+    this.handleStartButton = this.handleStartButton.bind(this);
 
   }
 
@@ -35,19 +40,95 @@ class Host extends React.Component {
       })
       .catch();
 
+      // Update answered players
+      var ansPlayers = [];
+      this.state.players.forEach(function(player) {
+        if(player.ans1 !== "" && player.ans2 !== "") {
+          ansPlayers.push(player)
+        }
+      });
+
+      this.setState(prevState => ({
+          answeredPlayers: ansPlayers,
+        }));
+
+      // Check if all players have answered
+      if(this.state.answeredPlayers.length === this.state.players.length && this.state.answeredPlayers.length > 0) {
+        // All players have answered, next stage
+        this.setState(prevState => ({
+          stage: 2,
+        }));
+      }
+
+  }
+
+  handleStartButton() {
+    fetch('/api/v1/resetanswer', {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'same-origin',
+    })
+
+    this.setState(prevState => ({
+          stage: 1,
+          answeredPlayers: [],
+        }));
+
   }
 
 
   render() {
+    let output = ""
+    switch(this.state.stage) {
+      case 0:
+        // Waiting for players
+        output = (<div><p>Waiting for all players...</p>
+          <ul>
+            {
+            this.state.players.map((player, index) =>
+              <li key={index}><p>{player.name}</p></li>)
+              }
+          </ul>
+
+        { this.state.players.length > 2 && 
+          <button onClick={this.handleStartButton}>Start</button>
+        }</div>);
+        break;
+
+      case 1:
+        // Answering questions
+        output = (<div><p>Waiting for players to answer questions...</p>
+
+        <ul>
+            {
+            this.state.answeredPlayers.map((player, index) =>
+              <li key={index}><p>{player.name}</p></li>)
+              }
+          </ul>
+          </div>);
+
+        break;
+
+      case 2:
+        // Display answers and wait for all votes
+        output = (<div><p>Waiting for players to vote on questions...</p>
+
+        <ul>
+            {
+            this.state.votedPlayers.map((player, index) =>
+              <li key={index}><p>{player.name}</p></li>)
+              }
+          </ul>
+          </div>);
+
+        break;
+
+      default:
+        // code block
+    }
     return (
       <div className="host">
-        <p>Waiting for all players...</p>
-        <ul>
-          {
-          this.state.players.map((player, index) =>
-            <li key={index}><p>{player.name}</p></li>)
-            }
-        </ul>
+        {output}
       </div>
     );
   }
